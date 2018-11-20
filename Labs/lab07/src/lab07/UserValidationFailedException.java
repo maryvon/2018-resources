891a85cda3baf01fb5bd30ddbe8ddf66466f5a9a
Alexandru Bondor <Alexandru.Bondor@catalysts.cc>
2018-11-20 17:15:20 +0200

    Add exception

---
+++ Labs/lab07/src/lab07/UserValidationFailedException.java
@@ -0,0 +1,5 @@
+package lab07;
+
+public class UserValidationFailedException extends Exception {
+
+}
---
+++ Labs/lab07/src/lab07/UserValidatorTest.java
@@ -0,0 +1,16 @@
+package lab07;
+
+import org.junit.Test;
+
+public class UserValidatorTest {
+
+	@Test
+	public void testValidUsers() {
+
+	}
+
+	@Test(expected = UserValidationFailedException.class)
+	public void testInvalidUser() {
+
+	}
+}
